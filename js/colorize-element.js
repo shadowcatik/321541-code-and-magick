6ffd284a45287f9c88b2e04a04366b49584ac8f0
OksanaMelnik <kyoshiniimura@gmail.com>
2017-02-21 00:07:28 +0300

    fix

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,10 +1,14 @@
 'use strict';
 
 window.colorizeElement = function (element, colors, property) {
+  var color;
   element.addEventListener('click', function () {
-    var computedStyle = getComputedStyle(element);
-    var color = computedStyle.getPropertyValue(property);
+    if (typeof element.style[property] === 'undefined') {
+      color = colors[0];
+    } else {
+      color = element.style[property];
+    }
     var getRandom = window.utils.getRandomElementExcept(colors, color);
-    element.style[property] = element[getRandom];
+    element.style[property] = getRandom;
   });
 };
