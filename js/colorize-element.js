5dd55dd7e3340efa4eabf315cdeaedc5200f668c
OksanaMelnik <kyoshiniimura@gmail.com>
2017-02-21 20:35:57 +0300

    add #17

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,8 +1,9 @@
 'use strict';
 
-window.colorizeElement = function (element, colors, property) {
+window.colorizeElement = (function (element, colors, property) {
   var color;
   var ENTER_KEY_CODE = 13;
+
   function colorElement() {
     if (typeof element.style[property] === 'undefined') {
       color = colors[0];
@@ -12,12 +13,14 @@ window.colorizeElement = function (element, colors, property) {
     var getRandom = window.utils.getRandomElementExcept(colors, color);
     element.style[property] = getRandom;
   }
+
   element.addEventListener('click', function () {
     colorElement();
   });
+
   element.addEventListener('keydown', function (evt) {
     if (evt.keyCode === ENTER_KEY_CODE) {
       colorElement();
     }
   });
-};
+})();
