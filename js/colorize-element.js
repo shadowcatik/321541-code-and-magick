7e13faacdf409260effba3c3d8ce6f654be95a3d
Oksana Melnik <kyoshiniimura@gmail.com>
2017-02-21 12:32:59 +0300

    Add 4p

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -2,7 +2,8 @@
 
 window.colorizeElement = function (element, colors, property) {
   var color;
-  element.addEventListener('click', function () {
+  var ENTER_KEY_CODE = 13;
+  function colorElement() {
     if (typeof element.style[property] === 'undefined') {
       color = colors[0];
     } else {
@@ -10,5 +11,13 @@ window.colorizeElement = function (element, colors, property) {
     }
     var getRandom = window.utils.getRandomElementExcept(colors, color);
     element.style[property] = getRandom;
+  }
+  element.addEventListener('click', function () {
+    colorElement();
+  });
+  element.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEY_CODE) {
+      colorElement();
+    }
   });
 };
